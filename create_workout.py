47b926a8719bd6bcfea79ac47d91d3d0335142b7
celiker <clkrv1@gmail.com>
2021-02-23 09:02:41 +0300

    base algo renewed. 2 seconds miss on 640 seconds workout

--- create_workout.py
+++ create_workout.py
@@ -15,15 +15,20 @@ def create_combination(name, expected_time):
 
 def create_workout(num_rounds, round_time, rest_time, comb_gap, combinations):
     workout = []
+    get_available_combs = lambda remaining_seconds: [comb for comb in combinations
+                                                     if comb["time"] < remaining_seconds]
     for i in range(num_rounds):
-        schedule = [EMPTY_PLACEHOLDER] * round_time
+        remaining_seconds = round_time - 4
+        combs = []
+        while len(available_combs:=get_available_combs(remaining_seconds)) > 0:
+            combs.append(random.choice(available_combs))
+            remaining_seconds -= combs[-1]["time"] + comb_gap
         second = 3 # for bell sound
-        while second < round_time:
-            combination = random.choice(combinations)
-            for i in range(second, min(second+combination["time"], round_time)):
-                schedule[i] = chr(ASCII_OFFSET+combination["id"])
-            second += combination["time"] + comb_gap
-        #schedule[::len(schedule)-1] = [chr(ASCII_OFFSET)]*2
+        schedule = [EMPTY_PLACEHOLDER] * round_time
+        for comb in combs:
+            for i in range(comb["time"]):
+                schedule[i+second] = chr(ASCII_OFFSET+comb["id"])
+            second += comb["time"] + comb_gap
         schedule[:3] = chr(ASCII_OFFSET)*3
         schedule[-1] = chr(ASCII_OFFSET)
         schedule += chr(ASCII_OFFSET)*2
